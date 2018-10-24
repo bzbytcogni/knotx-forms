01da5c69e5fa23395e2809daa0039d4b6ade9807
Tomasz Michalak <tomek.michalak@gmail.com>
2018-10-24 11:31:38 +0200

    Update api/src/main/java/io/knotx/forms/api/FormFragmentConstants.java
    Co-Authored-By: bmalecki <bmalecki995@gmail.com>

--- api/src/main/java/io/knotx/forms/api/FormFragmentConstants.java
+++ api/src/main/java/io/knotx/forms/api/FormFragmentConstants.java
@@ -1,6 +1,6 @@
 package io.knotx.forms.api;
 
-public class FormFragmentConstants {
+public final class FormFragmentConstants {
   public static final String FRAGMENT_FORM_CONTEXT = "form";
 
   private FormFragmentConstants() {}
