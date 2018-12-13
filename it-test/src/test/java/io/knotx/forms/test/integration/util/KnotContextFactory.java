2ab26a201f5075d3e0c20f278e465a402f30cf2d
Maciej Laskowski <skejven@gmail.com>
2018-12-13 15:58:42 +0100

    Cleanup tests

--- it-test/src/test/java/io/knotx/forms/test/integration/util/KnotContextFactory.java
+++ it-test/src/test/java/io/knotx/forms/test/integration/util/KnotContextFactory.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.knotx.forms.core.util;
+package io.knotx.forms.test.integration.util;
 
 import io.knotx.dataobjects.ClientRequest;
 import io.knotx.dataobjects.ClientResponse;
